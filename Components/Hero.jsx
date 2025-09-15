9434a04ed12de8e3d3f9e55672f628a4af0fd641
SomPNG <psomansu@gmail.com>
2025-09-15 19:13:31 +0530

    added hero section

---
+++ Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+        <section>
+            <div className='container'>
+                <div className='grid grid-cols-1 md:grid-cols-2'>
+
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Hero
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import React from "react"
+import Hero from "../Components/Hero"
 
 function App() {
 
   return (
-    <div>
-      
+    <div className="overflow-x-hidden">
+      <Hero/>
     </div>
   )
 }
